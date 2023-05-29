8c304cc066b4c949a1dc95dac5ef69c6444b0664
Jose Real <115721093+Jrealmorillo@users.noreply.github.com>
2023-05-29 19:07:12 +0000

    added intro feature and scroll bar hidden/showed

--- src/js/layout.js
+++ src/js/layout.js
@@ -36,6 +36,13 @@ const Layout = () => {
 		};
 	  }, [showIntro]);
 
+	  useEffect(() => {
+		document.body.classList.add("overflow-hidden");
+	  		return () => {
+		  document.body.classList.remove("overflow-hidden");
+		};
+	  }, []);
+
 	//the basename is used when your project is published in a subdirectory and not in the root of the domain
 	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
 	const basename = process.env.BASENAME || "";
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,10 +3,15 @@ import "../../styles/index.css";
 import { CharacterList } from "../component/characterList";
 import { PlanetList } from "../component/planetList";
 import { VehicleList } from "../component/vehicleList";
+import { useEffect } from "react";
 
 
 export const Home = () => {
 
+
+	useEffect(() => {
+		document.body.classList.remove("overflow-hidden");
+	  }, []);
 
 	return (
 		<>		
